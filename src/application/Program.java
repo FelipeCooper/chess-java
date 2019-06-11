9e95ef4e5808d3683312d8ee1deb20bd2ca5738e
FelipeCooper <hoonter69@gmail.com>
2019-06-11 16:21:29 -0300

    0.2 Created class: ChessMatch, ChessPiece, Color, UI

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,13 @@
 package application;
 
+import chess.ChessMatch;
+
 public class Program {
 
 	public static void main(String[] args) {
 		System.out.print("Chess game init");
-
+			ChessMatch chessmatch= new ChessMatch();
+			UI.printboard(chessmatch.getPieces());
 	}
 
 }
