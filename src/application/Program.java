30e6928af3f5e2cd65845119a6ab914d056e75bc
FelipeCooper <hoonter69@gmail.com>
2019-06-11 21:33:24 -0300

    0.3 Created all Pieces, Set Initial Setup

--- src/application/Program.java
+++ src/application/Program.java
@@ -5,7 +5,7 @@ import chess.ChessMatch;
 public class Program {
 
 	public static void main(String[] args) {
-		System.out.print("Chess game init");
+		System.out.println("Chess game init");
 			ChessMatch chessmatch= new ChessMatch();
 			UI.printboard(chessmatch.getPieces());
 	}
--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -1,12 +1,20 @@
 package chess;
 
 import boardgame.Board;
+import boardgame.Position;
+import chess.pieces.Bishop;
+import chess.pieces.King;
+import chess.pieces.Knight;
+import chess.pieces.Pawn;
+import chess.pieces.Queen;
+import chess.pieces.Rook;
 
 public class ChessMatch {
 	private Board board;
 	
 	public ChessMatch(){
 		board = new Board( 8, 8);
+		initialSetup();
 	}
 	
 	public ChessPiece[][] getPieces(){
@@ -17,6 +25,31 @@ public class ChessMatch {
 			}
 		}
 		return mat;
+	}
+	private void initialSetup() {
+		Color color = Color.BLACK;
+		int j = 0;
+		for(int i = 0; i<board.getColumns();i++) {
+			board.placePiece(new Pawn(board,Color.BLACK), new Position(1,i));
+		}
+		for(int i = 0; i<board.getColumns();i++) {
+			board.placePiece(new Pawn(board,Color.WHITE), new Position(6,i));
+		}
+		for (int i=0; i <= 1; i++) {
+			
+			board.placePiece(new Rook(board,color), new Position(j,0));
+			board.placePiece(new Knight(board,color), new Position(j,1));
+			board.placePiece(new Bishop(board,color), new Position(j,2));
+			board.placePiece(new Queen(board,color), new Position(j,3));
+			board.placePiece(new King(board,color), new Position(j,4));
+			board.placePiece(new Bishop(board,color), new Position(j,5));
+			board.placePiece(new Knight(board,color), new Position(j,6));
+			board.placePiece(new Rook(board,color), new Position(j,7));
+			j = 7;
+			color = Color.WHITE;
+		}
+		
+		
 	}
 	
 	
